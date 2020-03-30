43eb5a9a0e2bc0150a4a50d5f8f5d74d7d4df2d2
alip67 <alip67@users.noreply.github.com>
2020-03-30 12:42:02 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 
 chmod 755 run.sh
 cd src/
-make
-time ./application ../input/complaints.csv ../output/generated_report.csv
+
+time ./application ../insight_testsuit/test_1/input/complaints.csv ../insight_testsuit/test_1/output/generated_report.csv
 cd ..
 
