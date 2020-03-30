508f24267b3f7834d7410d5ee8961ba70a6daefc
alip67 <alip67@users.noreply.github.com>
2020-03-30 12:44:18 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. 
 
-chmod 755 run.sh
-cd src/
 
+cd src/
+chmod 755 run.sh
 time ./application ../insight_testsuit/test_1/input/complaints.csv ../insight_testsuit/test_1/output/generated_report.csv
 cd ..
 
