46ed1d5a0e2f976e2d04b3591ffddfe2cfe59930
alip67 <alip67@users.noreply.github.com>
2020-04-01 14:20:46 -0400

    delete application file

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 #
 # Use this shell script to compile (if necessary) your code and then execute it. 
 
-chmod 755 run.sh
+
 cd src/
-#make
+make
 time ./application ../input/complaints.csv ../output/report.csv
 cd ..
 
